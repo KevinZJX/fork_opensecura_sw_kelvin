5971487d5ae791d7c5fb4d33a784537912895f8b
Alex Van Damme <atv@google.com>
2024-05-15 13:50:53 -0700

    Improvements to elementwise_add_{s8,s16}
    - Implement the quantized multiplication with less instructions.
    
    Change-Id: I31b7b7b742fad91be7aebe6a2970c3519501e115

--- tflm/opt/util.h
+++ tflm/opt/util.h
@@ -25,22 +25,17 @@
 
 // Use this in place of Tensorflow's
 // MultiplyByQuantizedMultiplierSmallerThanOneExp
-#define rescale_internal(Vd, Vs, Vscratch, mult, shift, offset, m) \
+#define rescale_internal(Vd, Vs, mult, shift, offset, m) \
   do {                                                             \
-    int32_t _shift = RIGHT_SHIFT(shift);                           \
     vdmulh_w_r_vx##m(Vd, Vs, mult);                                \
-    vdup_w_x##m(Vscratch, -_shift);                                \
-    vand_vv##m(Vscratch, Vscratch, Vd);                            \
-    vsra_w_vx##m(Vscratch, Vscratch, 31);                          \
-    vadd_w_vv##m(Vd, Vd, Vscratch);                                \
-    vsha_w_r_vx##m(Vd, Vd, _shift);                                \
+    vsha_w_r_vx##m(Vd, Vd, -shift);                                \
     vadd_w_vx##m(Vd, Vd, offset);                                  \
   } while (0);
 
-#define rescale(Vd, Vs, Vscratch, mult, shift, offset) \
-  rescale_internal(Vd, Vs, Vscratch, mult, shift,      \
+#define rescale(Vd, Vs, mult, shift, offset) \
+  rescale_internal(Vd, Vs, mult, shift,      \
                    offset, );  // NOLINT(whitespace/parens)
-#define rescale_m(Vd, Vs, Vscratch, mult, shift, offset) \
-  rescale_internal(Vd, Vs, Vscratch, mult, shift, offset, _m);
+#define rescale_m(Vd, Vs, mult, shift, offset) \
+  rescale_internal(Vd, Vs, mult, shift, offset, _m);
 
 #endif  // TFLM_OPT_UTIL_H_
